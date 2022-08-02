cb3031daf7f07588483199a0846722394131f5fc
seungchae1 <s2125@e-mirim.hs.kr>
2022-08-02 10:43:59 +0900

    Title 영화제목으로 수정

--- app/src/main/java/kr/hs/emirim/s2125/mirim_project_0802_gridview/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2125/mirim_project_0802_gridview/MainActivity.java
@@ -19,6 +19,12 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.error, R.drawable.hunt, R.drawable.mini, R.drawable.topgun, R.drawable.summer,
             R.drawable.dr, R.drawable.exo, R.drawable.kn, R.drawable.pp, R.drawable.w, R.drawable.error,
             R.drawable.hunt, R.drawable.mini, R.drawable.topgun, R.drawable.summer};
+    String[] posterName={"극장판 도라에몽-진구의 우주소전쟁 리틀스타워즈 2021 ","비욘드 라이브 더 무비 : 엑소의 사다리 타고 세계여행3",
+    "명탐정 코난-할로윈의 신부","뽀로로 극장판 드래곤캐슬 대모험","외계+인 1부","시맨틱 에러-더 무비","헌트","미니언즈2","탑건-매버릭", "썸머 필름을 타고!",
+            "극장판 도라에몽-진구의 우주소전쟁 리틀스타워즈 2021 ","비욘드 라이브 더 무비 : 엑소의 사다리 타고 세계여행3",
+            "명탐정 코난-할로윈의 신부","뽀로로 극장판 드래곤캐슬 대모험","외계+인 1부","시맨틱 에러-더 무비","헌트","미니언즈2","탑건-매버릭", "썸머 필름을 타고!",
+            "극장판 도라에몽-진구의 우주소전쟁 리틀스타워즈 2021 ","비욘드 라이브 더 무비 : 엑소의 사다리 타고 세계여행3",
+            "명탐정 코난-할로윈의 신부","뽀로로 극장판 드래곤캐슬 대모험","외계+인 1부","시맨틱 에러-더 무비","헌트","미니언즈2","탑건-매버릭", "썸머 필름을 타고!"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onClick(View view) {
                     AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
-                    dlg.setTitle("Large Poster");
+                    dlg.setTitle(posterName[pos]);
                     dlg.setIcon(R.drawable.movie);
                     View dlgView = View.inflate(MainActivity.this, R.layout.dialog,null);
                     ImageView imgvDlg = dlgView.findViewById(R.id.imgv_dlg);
